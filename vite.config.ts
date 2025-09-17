17c9b8dcdd93a8132a1b122cc9934279033a0a21
luis-tapia12 <j.contreras.t@hotmail.com>
2025-09-16 22:40:02 -0600

    build: add configuration for tree-shaking

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,35 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import { extname, relative, resolve } from 'path';
+import react from '@vitejs/plugin-react-swc';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss()],
   build: {
     copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob
+          .sync('lib/**/*.{ts,tsx}', {
+            ignore: ['lib/**/*.d.ts'],
+          })
+          .map((file) => [
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
     },
   },
 });
