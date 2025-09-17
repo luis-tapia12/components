2e72f0131b7b66198036bcc3e4931dff25f96778
luis-tapia12 <j.contreras.t@hotmail.com>
2025-09-16 21:36:19 -0600

    chore: initialize vite in library mode

--- src/main.tsx
+++
@@ -1,3 +0,0 @@
-import { createRoot } from "react-dom/client";
-
-createRoot(document.getElementById("root")!).render(<div>__COMPONENTS__</div>);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
+});
